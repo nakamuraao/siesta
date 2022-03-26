e47b449075672b515fcc571ff2cef44fd42d8ad8
nakamuraao <williamhuang0831@gmail.com>
2022-03-26 14:22:42 +0800

    added tc and yt member

--- UID.js
+++ UID.js
@@ -36,6 +36,7 @@ client.on('interactionCreate', async interaction => {
 client.on('messageCreate', async msg => {
 	if (msg.author.bot) return;
 
+	//normal
 	if (msg.channelId === '950020050792894464'){
 
 		const webhook = new WebhookClient({url : 'https://discord.com/api/webhooks/952935493153222756/7mtoiRicyc-4LJ5zmvMhedcXDzUFhi9pw_tEncTZ7UEhYB5vabMZmfdqo72GqNp6Se_J'});
@@ -46,6 +47,33 @@ client.on('messageCreate', async msg => {
 		})
 
 	}
+
+	//tc
+	if (msg.channelId === '951115920607154186'){
+
+		const webhook = new WebhookClient({url : 'https://discord.com/api/webhooks/957162016785711134/TIMHVfMweK5-w5pgkdLq6LQ1Njx0i6uMYiv8niBa2SJ79wJGhOOreoVaTVUIGEXdg2B9'});
+		webhook.send({
+			content: `${msg.content}`,
+			username: `${msg.author.tag}`,
+			avatarURL: `${msg.author.avatarURL()}`
+		})
+
+	}
+
+	//yt
+	if (msg.channelId === '951116441497792664'){
+
+		const webhook = new WebhookClient({url : 'https://discord.com/api/webhooks/957162169127022652/fOXsGHo6tX8QHv85EOmnkSg26dNSRMmRt76twEZWURb0iu6UAuYfyA-eJHgO8GXXf5Qa'});
+		webhook.send({
+			content: `${msg.content}`,
+			username: `${msg.author.tag}`,
+			avatarURL: `${msg.author.avatarURL()}`
+		})
+
+	}
+
+
+	
 })
 
 client.login(token);
