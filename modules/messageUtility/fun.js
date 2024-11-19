f8056991ee7e834788a18488479b4cd001f792d6
Oliver Mak <oliver139.working@gmail.com>
2024-11-19 22:20:21 +0800

    Support more meal

--- modules/messageUtility/fun.js
+++ modules/messageUtility/fun.js
@@ -10,11 +10,24 @@ module.exports = {
       msg.channel.send(`${num}%`);
     } else if (msg.content.includes('抽籤') && await Obj_cre.findChannel(msg.channelId)) {
       omikuji(msg);
-    } else if (msg.content.includes('晚餐吃什麼') && await Obj_cre.findChannel(msg.channelId)) {
-      const res = dinnerTonight();
-      msg.reply(res === "不要吃" ? `不要吃晚餐` : `今天晚餐吃 ${res}`);
     } else if (msg.content === `<@${config.cid}>我婆` || msg.content === `<@!${config.cid}>我婆`) {
       msg.reply(isOwner(msg.author.id) ? '沒錯♥' : '婆你個大頭 醒');
+    } else if (msg.content.includes('吃什麼') && await Obj_cre.findChannel(msg.channelId)) {
+      const mealMatch = [
+        "早餐", "午餐", "晚餐", "宵夜",
+        "早上", "上午", "中午", "下午", "晚上",
+        "今晚",
+        "明早", "明天", "明晚",
+        "待會",
+      ];
+      for (let index = 0; index < mealMatch.length; index++) {
+        const matchText = `${mealMatch[index]}吃什麼`;
+        if (msg.content.includes(matchText)) {
+          const res = dinnerTonight();
+          msg.reply(res === "不要吃" ? `不要吃晚餐` : `${mealMatch[index]}就吃${res}`);
+          break;
+        }
+      }
     }
   }
 };
