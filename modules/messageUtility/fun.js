97de9f4643f98df4fb44cf685007d68ae6ade872
Oliver Mak <oliver139.working@gmail.com>
2024-11-21 16:06:50 +0800

    Add more dinner time and Fix missed condition (#5)
    * Add more meal time
    
    * Add back channel condition

--- modules/messageUtility/fun.js
+++ modules/messageUtility/fun.js
@@ -12,23 +12,22 @@ module.exports = {
       omikuji(msg);
     } else if (msg.content === `<@${config.cid}>我婆` || msg.content === `<@!${config.cid}>我婆`) {
       msg.reply(isOwner(msg.author.id) ? '沒錯♥' : '婆你個大頭 醒');
-    // } else if ((msg.content.includes('吃什麼') || msg.content.includes('食咩')) && await Obj_cre.findChannel(msg.channelId)) {
-    } else if ((msg.content.includes('吃什麼') || msg.content.includes('食咩'))) {
+    } else if ((msg.content.includes('吃什麼') || msg.content.includes('食咩')) && await Obj_cre.findChannel(msg.channelId)) {
       const mealMatch = [
-        "早餐", "午餐", "晚餐", "宵夜",
-        "早上", "朝早", "上午", "中午", "下午", "晏晝", "晚上", "夜晚",
-        "今朝", "今晚",
-        "明早", "明天", "明晚", "聽朝", "聽日", "聽晚",
+        "早餐", "中餐", "午餐", "下午茶", "晚餐", "宵夜",
+        "早上", "朝早", "上午", "上晝", "中午", "晏晝", "下午", "下晝", "晚上", "夜晚",
+        "今早", "今朝", "今晚",
+        "明天", "聽日", "明早", "聽朝", "明晚", "聽晚",
         "待會", "陣間",
       ];
       for (let index = 0; index < mealMatch.length; index++) {
         if (msg.content.includes(`${mealMatch[index]}吃什麼`)) {
           const res = dinnerTonight();
-          msg.reply(res === "不要吃" ? `不要吃` : `${mealMatch[index]}就吃${res}`);
+          msg.reply(res === "不要吃" ? "不要吃" : res === "自己想啦" ? `${mealMatch[index]}吃什麼？自己想啦` : `${mealMatch[index]}就吃${res}`);
           break;
         } else if (msg.content.includes(`${mealMatch[index]}食咩`)) {
           const res = dinnerTonight();
-          msg.reply(res === "不要吃" ? `唔好食` : `${mealMatch[index]}就食${res}`);
+          msg.reply(res === "不要吃" ? "唔好食" : res === "自己想啦" ? `${mealMatch[index]}食咩？咁大個人自己諗啦` : `${mealMatch[index]}就食${res}`);
           break;
         }
       }
