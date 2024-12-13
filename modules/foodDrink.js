ffebe25f6accabe7f997a3b92c07e5c54fd4f4b6
Oliver Mak <oliver139.working@gmail.com>
2024-12-13 21:34:09 +0800

    Refactoring, new feature, new meal (#8)
    * Modularize, add set meal function
    
    * Remove `console.log`
    
    * Add back condition
    
    * Update menu
    
    * typo
    
    * Support more word and add white son

---
+++ modules/foodDrink.js
@@ -0,0 +1,155 @@
+const { default: randomFn } = require("random");
+
+const dinner = require('../data/dinner');
+const drinks = require('../data/drinks');
+const dict = require("../data/dictionary");
+
+function pickFood() {
+  return randomFn.choice(dinner);
+}
+function pickDrinks() {
+  return randomFn.choice(drinks);
+}
+function pickFoodDrink(type, testMode) {
+  switch (type) {
+    case "food":
+      if (testMode === 1) return "吃什麼自己想啦";
+      if (testMode === 2) return "不要吃";
+      return pickFood();
+
+    case "drink":
+      if (testMode === 1) return "喝什麼自己想啦";
+      if (testMode === 2) return "不要喝";
+      return pickDrinks();
+  }
+}
+
+// #region : 菜單機率
+function getMenuStat() {
+  const foodTotalCount = dinner.good.length + dinner.strange.length;
+  const foodGoodProbi = (dinner.good.length * 100 / foodTotalCount).toFixed(2);
+  const foodStrangeProbi = (dinner.strange.length * 100 / foodTotalCount).toFixed(2);
+
+  const drinksTotalCount = drinks.good.length + drinks.strange.length;
+  const drinksGoodProbi = (drinks.good.length * 100 / drinksTotalCount).toFixed(2);
+  const drinksStrangeProbi = (drinks.strange.length * 100 / drinksTotalCount).toFixed(2);
+
+  return [
+    `:cooking:食物菜單裡有 **${foodTotalCount}** 項東西，其中：`,
+    `- 正常的東西：有 **${dinner.good.length}** 項，抽到的機率約為 **${foodGoodProbi}%**`,
+    `- 奇怪的東西：有 **${dinner.strange.length}** 項，抽到的機率約為 **${foodStrangeProbi}%**`,
+    "",
+    `:bubble_tea:飲料菜單裡有 **${drinksTotalCount}** 項東西，其中：`,
+    `- 正常的東西：有 **${drinks.good.length}** 項，抽到的機率約為 **${drinksGoodProbi}%**`,
+    `- 奇怪的東西：有 **${drinks.strange.length}** 項，抽到的機率約為 **${drinksStrangeProbi}%**`,
+  ].join("\n");
+}
+//  #endregion
+
+// #region : 吃什麼喝什麼
+
+// 時間和類別
+const mealMatch = [
+  "早餐", "中餐", "午餐", "下午茶", "晚餐", "宵夜",
+  "早上", "朝早", "上午", "上晝", "中午", "晏晝", "下午", "下晝", "晚上", "夜晚",
+  "今早", "今朝", "今晚",
+  "明天", "聽日", "明早", "聽朝", "明晚", "聽晚",
+  "待會", "陣間",
+  "前菜", "主菜", "正餐", "甜品", "零食", "小吃",
+];
+
+// 觸發的文字
+function isAskingMeal(msg) {
+  const matchPatterns = [
+    "吃什麼",
+    "吃甚麼",
+    "食咩",
+    "食乜",
+    "喝什麼",
+    "喝甚麼",
+    "飲咩",
+    "飲乜",
+    "來個套餐",
+    "來份套餐",
+    "要個套餐",
+    "要份套餐",
+  ];
+  return matchPatterns.some(item => msg.includes(item));
+}
+
+// 抽
+function eatDrinkWhat(msg, testMode) {
+  const matchPatterns = [
+    { key: `喝什麼`, type: "drink", lang: "cn" },
+    { key: `喝甚麼`, type: "drink", lang: "cn" },
+    { key: `飲咩`, type: "drink", lang: "canto" },
+    { key: `飲乜`, type: "drink", lang: "canto" },
+    { key: `來個套餐`, type: "setMeal", lang: "cn" },
+    { key: `來份套餐`, type: "setMeal", lang: "cn" },
+    { key: `要個套餐`, type: "setMeal", lang: "canto" },
+    { key: `要份套餐`, type: "setMeal", lang: "canto" },
+  ];
+
+  const replyTemplate = new Map([
+    ["drink", { cn: `就喝{drink}`, canto: `就飲{drink}` }],
+    ["setMeal", {
+      food: { cn: `那就吃{food}`, canto: `咁就食{food}` },
+      drink: { cn: `飲料的話就要{drink}`, canto: `野飲就要{drink}` },
+    }],
+  ]);
+
+  const meal = mealMatch.find(term => msg.includes(term));
+  if (meal) {
+    matchPatterns.push(
+      { key: `${meal}吃什麼`, type: "food", lang: "cn" },
+      { key: `${meal}吃甚麼`, type: "food", lang: "cn" },
+      { key: `${meal}食咩`, type: "food", lang: "canto" },
+      { key: `${meal}食乜`, type: "food", lang: "canto" },
+    );
+    replyTemplate.set("food", { cn: `${meal}就吃{food}`, canto: `${meal}就食{food}` });
+  }
+
+  const match = matchPatterns.find(({ key }) => msg.includes(key));
+
+  if (!match) return null;
+
+  try {
+    let choice, choiceProcessed, reply;
+    switch (match.type) {
+      case "food":
+      case "drink":
+        choice = pickFoodDrink(match.type, testMode);
+        reply = dict[match.type].get(choice)?.[match.lang] ?? replyTemplate.get(match.type)[match.lang];
+        return reply
+          .replace(`{meal}`, meal)
+          .replace(`{${match.type}}`, choice);
+
+      case "setMeal":
+        choice = {
+          food: pickFoodDrink("food", testMode),
+          drink: pickFoodDrink("drink", testMode),
+        };
+        reply = {
+          food: dict.setMeal.get(choice.food)?.[match.lang] ?? replyTemplate.get("setMeal")["food"][match.lang],
+          drink: dict.setMeal.get(choice.drink)?.[match.lang] ?? replyTemplate.get("setMeal")["drink"][match.lang],
+        };
+        return Object.values(reply).join("\n")
+          .replace(`{food}`, choice.food)
+          .replace(`{drink}`, choice.drink);
+    }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+// #endregion
+
+module.exports = {
+  getMenuStat,
+  pickFood,
+  pickDrinks,
+  pickFoodDrink,
+  isAskingMeal,
+  eatDrinkWhat,
+};
--- modules/messageUtility/fun.js
+++ modules/messageUtility/fun.js
@@ -1,109 +1,14 @@
 const config = require('../../config.json');
-const { omikuji, isOwner, dinnerTonight, pickDrinks, pickFoodDrink } = require('../utility');
+const { omikuji, isOwner } = require('../utility');
 const botzoneDB = require('../dbFunction/botChannel');
 const Obj_cre = new botzoneDB.botzone;
 const { default: randomFn } = require("random");
-
-// #region : 吃什麼喝什麼
-
-// 時間和類別
-const mealMatch = [
-  "早餐", "中餐", "午餐", "下午茶", "晚餐", "宵夜",
-  "早上", "朝早", "上午", "上晝", "中午", "晏晝", "下午", "下晝", "晚上", "夜晚",
-  "今早", "今朝", "今晚",
-  "明天", "聽日", "明早", "聽朝", "明晚", "聽晚",
-  "待會", "陣間",
-  "前菜", "主菜", "正餐", "甜品", "零食", "小吃",
-];
-
-// 觸發的文字
-function isAskingMeal(msg) {
-  const matchPatterns = [
-    "吃什麼",
-    "食咩",
-    "喝什麼",
-    "飲咩",
-    // "來份套餐",
-    // "要個套餐",
-  ];
-  return matchPatterns.some(item => msg.includes(item));
-}
-
-// 選擇餐點
-function eatDrinkWhat(msg) {
-  const matchPatterns = [
-    { key: `喝什麼`, type: "drink", lang: "cn", ans: "就喝{drink}" },
-    { key: `飲咩`, type: "drink", lang: "canto", ans: "就飲{drink}" },
-    // { key: `來份套餐`, type: "set", lang: "cn", ans: "那就吃{food}\n飲料的話就要{drink}" },
-    // { key: `要個套餐`, type: "set", lang: "canto", ans: "咁就食{food}\n野飲就要{drink}" },
-  ];
-
-  const meal = mealMatch.find(term => msg.includes(term));
-  if (meal) {
-    matchPatterns.push(
-      { key: `${meal}吃什麼`, type: "food", lang: "cn", ans: `${meal}就吃{food}` },
-      { key: `${meal}食咩`, type: "food", lang: "canto", ans: `${meal}就食{food}` },
-    );
-  }
-
-  const match = matchPatterns.find(({ key }) => msg.includes(key));
-
-  if (!match) return null;
-
-  const dict = require("../../data/dictionary");
-  try {
-    let choice, choiceProcessed;
-    switch (match.type) {
-      case "food":
-      case "drink":
-        choice = pickFoodDrink(match.type);
-        choiceProcessed = dict[match.type].get(choice)?.[match.lang] ?? choice;
-        return match.ans
-          .replace(`{meal}`, meal)
-          .replace(`{${match.type}}`, choiceProcessed);
-
-      case "set":
-        choice = {
-          food: pickFoodDrink("food"),
-          drink: pickFoodDrink("drink"),
-        };
-        choiceProcessed = {
-          food: dict.food.get(choice.food)?.[match.lang] ?? choice.food,
-          drink: dict.drink.get(choice.drink)?.[match.lang] ?? choice.drink,
-        };
-        return match.ans
-          .replace(`{food}`, choiceProcessed.food)
-          .replace(`{drink}`, choiceProcessed.drink);
-    }
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
-//  #endregion
+const { isAskingMeal, eatDrinkWhat, getMenuStat } = require("../foodDrink");
 
 module.exports = {
   async execute(msg) {
     if (msg.content === "菜單機率" && await Obj_cre.findChannel(msg.channelId)) {
-      const dinner = require("../../data/dinner");
-      const foodTotalCount = dinner.good.length + dinner.strange.length;
-      const foodGoodProbi = (dinner.good.length * 100 / foodTotalCount).toFixed(2);
-      const foodStrangeProbi = (dinner.strange.length * 100 / foodTotalCount).toFixed(2);
-
-      const drinks = require("../../data/drinks");
-      const drinksTotalCount = drinks.good.length + drinks.strange.length;
-      const drinksGoodProbi = (drinks.good.length * 100 / drinksTotalCount).toFixed(2);
-      const drinksStrangeProbi = (drinks.strange.length * 100 / drinksTotalCount).toFixed(2);
-
-      msg.reply([
-        `:cooking:食物菜單裡有 **${foodTotalCount}** 項東西，其中：`,
-        `- 正常的東西：有 **${dinner.good.length}** 項，抽到的機率約為 **${foodGoodProbi}%**`,
-        `- 奇怪的東西：有 **${dinner.strange.length}** 項，抽到的機率約為 **${foodStrangeProbi}%**`,
-        "",
-        `:bubble_tea:飲料菜單裡有 **${drinksTotalCount}** 項東西，其中：`,
-        `- 正常的東西：有 **${drinks.good.length}** 項，抽到的機率約為 **${drinksGoodProbi}%**`,
-        `- 奇怪的東西：有 **${drinks.strange.length}** 項，抽到的機率約為 **${drinksStrangeProbi}%**`,
-      ].join("\n"));
+      msg.reply(getMenuStat());
     } else if (msg.content.includes('機率') && (await Obj_cre.findChannel(msg.channelId) || isOwner(msg.author.id))) {
       const num = randomFn.int(0, 100);
       msg.channel.send(`${num}%`);
@@ -114,13 +19,29 @@ module.exports = {
     } else if (isAskingMeal(msg.content) && !msg.author.bot && await Obj_cre.findChannel(msg.channelId)) {
       const choice = eatDrinkWhat(msg.content);
 
-      // Error Handle
+      // Error Handling
       if (choice === false) {
         msg.reply("累了，你自己想吧");
         return;
       }
 
       if (choice !== null) msg.reply(choice);
+    } else if (msg.content === "菜單測試" && isOwner(msg.author.id)) {
+      const output = [
+        eatDrinkWhat("早餐吃什麼"),
+        eatDrinkWhat("早餐吃什麼", 1),
+        eatDrinkWhat("早餐吃什麼", 2),
+        "-----------------------------",
+        eatDrinkWhat("喝什麼"),
+        eatDrinkWhat("喝什麼", 1),
+        eatDrinkWhat("喝什麼", 2),
+        "-----------------------------",
+        eatDrinkWhat("來份套餐"),
+        eatDrinkWhat("來份套餐", 1),
+        eatDrinkWhat("來份套餐", 2),
+      ];
+      console.debug(output);
+      msg.reply(output.join("\n"));
     }
   }
 };
