de915da23aaacb8215d62b41ca14a1e9ef2ba460
Oliver Mak <oliver.mak@naxos.com>
2026-06-09 15:55:14 +0800

    兄弟！我把`cmdreg`的輸出弄好看了！

--- cmdreg.js
+++ cmdreg.js
@@ -1,4 +1,5 @@
 const fs = require('node:fs');
+const c = require('ansis');
 const { REST, Routes } = require('discord.js');
 const { cid, token } = require('./config.json');
 
@@ -12,5 +13,9 @@ for (const file of commandFiles) {
 
 const rest = new REST().setToken(token);
 rest.put(Routes.applicationCommands(cid), { body: commands })
-  .then(() => console.log('Successfully registered application commands.'))
+  .then(() => {
+    console.log(c.green.bold`Successfully registered application commands.\n`);
+    console.log(c.cyan`List of commands:`);
+    commands.forEach(cmd => console.log(`${c.cyan`-`} ${c.yellow(cmd.name)}: ${cmd.description}`));
+  })
   .catch(console.error);
